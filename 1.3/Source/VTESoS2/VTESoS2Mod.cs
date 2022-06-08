f4e4f8a2f8da738e99ff39145ab5fb0542d96785
Joshua <legodude17@users.noreply.github.com>
2022-06-08 13:26:47 -0700

    Rewrite harmony patch

--- 1.3/Source/VTESoS2/VTESoS2Mod.cs
+++ 1.3/Source/VTESoS2/VTESoS2Mod.cs
@@ -1,5 +1,6 @@
 ﻿using HarmonyLib;
 using RimWorld;
+using SaveOurShip2;
 using Verse;
 
 namespace VTESoS
@@ -11,28 +12,48 @@ namespace VTESoS
         public VTESoS2Mod(ModContentPack content) : base(content)
         {
             Harm = new Harmony("vanillaexpanded.vtexe.sos2");
-            Harm.Patch(AccessTools.Method(typeof(CompShuttleCosmetics), nameof(CompShuttleCosmetics.ChangeShipGraphics)),
-                new HarmonyMethod(GetType(), nameof(ChangeShipGraphics_Prefix)));
+            LongEventHandler.ExecuteWhenFinished(delegate
+            {
+                Harm.Patch(AccessTools.Method(typeof(ShipInteriorMod2), nameof(ShipInteriorMod2.SceneLoaded)),
+                    new HarmonyMethod(GetType(), nameof(SceneLoaded_Prefix)), new HarmonyMethod(GetType(), nameof(SceneLoaded_Postfix)));
+            });
         }
 
-        public static bool ChangeShipGraphics_Prefix(ThingWithComps parent, CompProperties_ShuttleCosmetics Props, bool triggeredByChange = false)
+        public static void SceneLoaded_Prefix(ref bool ___loadedGraphics, out bool __state)
         {
-            if (triggeredByChange) parent.GetComp<CompShuttleCosmetics>().whichVersion = FloatMenuWithCallback.whichOptionWasChosen;
+            __state = ___loadedGraphics;
+            ___loadedGraphics = true;
+        }
 
-            var whichVersion = parent.GetComp<CompShuttleCosmetics>().whichVersion;
-            if (parent is Pawn pawn)
+        public static void SceneLoaded_Postfix(bool __state, ref bool ___loadedGraphics)
+        {
+            if (__state) return;
+            ___loadedGraphics = false;
+            foreach (var thingToResolve in CompShuttleCosmetics.GraphicsToResolve.Keys)
             {
-                if (!pawn.Drawer.renderer.graphics.AllResolved) pawn.Drawer.renderer.graphics.ResolveAllGraphics();
-                pawn.Drawer.renderer.graphics.nakedGraphic = Props.graphicsHover[whichVersion].Graphic;
-                pawn.Drawer.renderer.graphics.ClearCache();
-            }
-            else
-            {
-                parent.graphicInt = Props.graphics[whichVersion].Graphic;
-                parent.DirtyMapMesh(parent.Map);
+                var graphicsResolved = new Graphic[CompShuttleCosmetics.GraphicsToResolve[thingToResolve].graphics.Count];
+                var graphicsHoverResolved = new Graphic[CompShuttleCosmetics.GraphicsToResolve[thingToResolve].graphicsHover.Count];
+                for (var i = 0; i < CompShuttleCosmetics.GraphicsToResolve[thingToResolve].graphics.Count; i++)
+                {
+                    var graphicData = CompShuttleCosmetics.GraphicsToResolve[thingToResolve].graphics[i];
+                    graphicsResolved[i] = GraphicDatabase.Get(graphicData.graphicClass, graphicData.texPath,
+                        (graphicData.shaderType ?? ShaderTypeDefOf.Cutout).Shader, graphicData.drawSize, graphicData.color, graphicData.colorTwo, graphicData,
+                        graphicData.shaderParameters);
+                }
+
+                for (var j = 0; j < CompShuttleCosmetics.GraphicsToResolve[thingToResolve].graphicsHover.Count; j++)
+                {
+                    var graphicData = CompShuttleCosmetics.GraphicsToResolve[thingToResolve].graphicsHover[j];
+                    graphicsHoverResolved[j] = GraphicDatabase.Get(graphicData.graphicClass, graphicData.texPath,
+                        (graphicData.shaderType ?? ShaderTypeDefOf.Cutout).Shader, graphicData.drawSize, graphicData.color, graphicData.colorTwo, graphicData,
+                        graphicData.shaderParameters);
+                }
+
+                CompShuttleCosmetics.graphics.Add(thingToResolve.defName, graphicsResolved);
+                CompShuttleCosmetics.graphicsHover.Add(thingToResolve.defName, graphicsHoverResolved);
             }
 
-            return false;
+            ___loadedGraphics = true;
         }
     }
 }
